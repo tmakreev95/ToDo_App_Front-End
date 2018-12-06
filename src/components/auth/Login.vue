bad9e4ea7ce371963ecb957b9f25723c0a0bb847
Todor Makreev <todor@webmotion.bg>
2018-12-06 21:40:34 +0200

    Initial Commit

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="flex-center">
+        <div class="login-form">
+            <h2 class="login-heading">Login</h2>
+            <form action="#" @submit.prevent="login">
+                <div class="form-control">
+                    <label for="email">Email</label>
+                    <input type="email" v-model="username" name="username" id="username" class="login-input">
+                </div>
+                <div class="form-control mb-more">
+                    <label for="password">Password</label>
+                    <input type="password" v-model="password" name="password" id="password" class="login-input">
+                </div>
+                <div class="form-control">
+                    <input type="submit" class="btn-submit" value="Login">
+                </div>
+            </form>
+        </div>
+    </div>    
+</template>
+<script>
+export default {
+    name: 'login',
+    data() {
+        return {
+            username: '',
+            password: '',
+        }
+    },
+    methods: {
+        login() {
+            this.$store.dispatch('retrieveToken', {
+                username: this.username,
+                password: this.password,
+            })
+            .then(response => {
+                this.$router.push({ name: 'todo' })
+            })
+        }
+    }
+}
+</script>
+
+
+
+
---
+++ src/main.js
@@ -0,0 +1,49 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Master from './components/layouts/Master'
+import { store } from './store/store'
+import VueRouter from 'vue-router'
+import routes from './routes/routes'
+
+window.eventBus = new Vue()
+
+Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history',
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.getters.loggedIn) {
+      next({
+        name: 'login',
+      })
+    } else {
+      next()
+    }
+  } else if (to.matched.some(record => record.meta.requiresVisitor)) {
+    if (store.getters.loggedIn) {
+      next({
+        name: 'todo',
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router: router,
+  store: store,
+  components: { Master },
+  template: '<Master/>'
+})
